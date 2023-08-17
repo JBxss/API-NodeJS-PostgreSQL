82d2f641e2656d91485ffdd463578ebf6ec40f38
JBxss <juanseb100@gmail.com>
2023-08-17 14:54:22 -0500

    [NEW] metodos GET y POST funcionando

---
+++ database/database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE firstapi;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(40),
+    email TEXT
+);
+
+INSERT INTO users (name, email) VALUES ('juan', 'juan@ibm.com'), ('cris', 'cris@abc.com');
--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,7 +1,32 @@
-const getUsers = (req, res) => {
-    res.send('users');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'firstapi',
+    port: '5432'
+})
+
+const getUsers = async (req, res) => {
+    const response = await pool.query('SELECT * FROM users');
+    res.status(200).json(response.rows);
+}
+
+const createUsers =  async (req, res) => {
+    const { name, email } = req.body;
+    const response = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+
+    console.log(response);
+    res.json({
+        message: 'User Added Succesfully',
+        body: {
+            user: {name, email}
+        }
+    })
 }
 
 module.exports = {
-    getUsers
+    getUsers,
+    createUsers
 }
