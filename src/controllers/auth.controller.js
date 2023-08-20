02b66a6f1a82ba939cd4e81e4961882ce93f88fe
JBxss <juanseb100@gmail.com>
2023-08-20 00:26:27 -0500

    [ADD] metodo de profile con JWT

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,47 +1,62 @@
-require('dotenv').config();
-const { Pool } = require('pg');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const { Pool } = require("pg");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'firstapi',
-    port: '5432'
-})
+  host: "localhost",
+  user: "postgres",
+  password: "postgres",
+  database: "firstapi",
+  port: "5432",
+});
 
 const registerUsers = async (req, res) => {
-    const { username, pass } = req.body;
-    let passHash = await bcrypt.hash(pass, 10);
+  const { username, pass } = req.body;
+  let passHash = await bcrypt.hash(pass, 10);
 
-    const response = await pool.query('INSERT INTO auth (username, pass) VALUES ($1, $2)', [username, passHash]);
-    console.log(response);
+  const response = await pool.query(
+    "INSERT INTO auth (username, pass) VALUES ($1, $2) RETURNING id",
+    [username, passHash]
+  );
+  
+  const newUserId = response.rows[0].id;
 
-    // jwt.sign({username, pass}, process.env.SECRET, {
-    //     expiresIn: 60*60*24
-    // });
+  const token = jwt.sign({ id: newUserId, user: username, password: pass, passHash: passHash }, process.env.SECRET, {
+    expiresIn: 60 * 60 * 24,
+  });
 
-    res.json({
-        message: 'User Added Succesfully',
-        auth: true,
-        body: {
-            user: {username, passHash}
-        }
-    })
-}
-
-const loginUsers = async (req, res) => {
-
-    
-}
+  res.json({
+    message: "User Added Succesfully",
+    auth: true,
+    body: {
+      user: { username, pass, token },
+    },
+  });
+};
 
 const profileUsers = async (req, res) => {
-    res.json("me");
-}
+
+  const token = req.headers['access-token'];
+  if(!token){
+    return res.status(401).json({
+      auth: false,
+      message: 'No token provided'
+    })
+  }
+
+  const decoded = jwt.verify(token, process.env.SECRET);
+  console.log(decoded)
+  res.json("me")
+
+};
+
+const loginUsers = async (req, res) => {};
+
+
 
 module.exports = {
-    registerUsers,
-    loginUsers,
-    profileUsers
-}
+  registerUsers,
+  loginUsers,
+  profileUsers,
+};
